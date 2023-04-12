319e734684f60eb03d3cec952a467a3d9d27af29
김동규 <tkillman@logiall.com>
2023-04-12 11:01:14 +0900

    빌드 수정 프리티어 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [count, setCount] = useState(0);
 
-  return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+    useEffect(() => {
+        const filePath = import.meta.env.MODE === 'production' ? '/assets/js' : '/src/assets/js';
+
+        const addScript = (sourcePath: string) => {
+            const sourcePathUrl = new URL(sourcePath, import.meta.url).href;
+            const isExist = !!document.querySelector(`script[src="${sourcePathUrl}"]`);
+            if (isExist) {
+                return;
+            }
+            const s = document.createElement('script');
+            s.setAttribute('src', sourcePathUrl);
+            s.setAttribute('defer', 'true');
+            document.body.appendChild(s);
+        };
+
+        addScript(`${filePath}/jquery-2.1.4.js`);
+        addScript(`${filePath}/modernizr.js`);
+        addScript(`${filePath}/velocity.min.js`);
+        addScript(`${filePath}/velocity.ui.min.js`);
+        addScript(`${filePath}/main.js`);
+    }, []);
+
+    return <div className='App'>App</div>;
 }
 
-export default App
+export default App;
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+    /** 빌드 옵션 */
+    build: {
+        /** 빌드 시 이전 결과물 삭제 옵션 */
+        emptyOutDir: true,
+    },
+    plugins: [
+        react(),
+        viteStaticCopy({
+            targets: [
+                {
+                    src: 'src/assets/js/*',
+                    dest: 'assets/js',
+                },
+            ],
+        }),
+    ],
+});
